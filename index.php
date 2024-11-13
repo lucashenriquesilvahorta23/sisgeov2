d9fd7e05844072193311c77352bed9c0bdc8e328
lucashenrique2311 <lucas.silva@doctorid.com.br>
2024-11-13 18:41:20 -0300

    att

--- index.php
+++ index.php
@@ -28,6 +28,8 @@ if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
 
 // Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+define('BASEPATH', __DIR__ . '/system/');
+
 
 // Ensure the current directory is pointing to the front controller's directory
 if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
